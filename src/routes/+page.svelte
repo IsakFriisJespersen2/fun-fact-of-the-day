6978a3f20768b82eb73155d561a9f5595baca5c7
Isak Friis-Jespersen <isak.friisjespersen2@gmail.com>
2024-03-05 22:16:11 +0100

    Added openai

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let fact: string | null = null;
+
+	const getFact = async (): Promise<string> => {
+		return await fetch('/api/get-fact').then((res) => res.json());
+	};
+
+	onMount(async () => {
+		fact = await getFact();
+	});
+
+</script>
+
+<main class="flex flex-col justify-center items-center h-full w-full space-y-8">
+	<section class="space-y-1">
+		<h1 class="text-5xl font-thin">Fun Fact Of The Day</h1>
+		<p class="flex justify-end font-thin">This is a fact generated with Chat-gpt</p>
+	</section>
+	<section class="w-1/2 h-96 font-mono px-6 py-8 border rounded-md">
+		{#if !!fact}
+			<p class="text-lg">{fact}</p>
+		{:else}
+			<p class="text-lg">...Loading</p>
+		{/if}
+	</section>
+</main>
--- src/routes/api/get-fact/+server.ts
+++ src/routes/api/get-fact/+server.ts
@@ -1,2 +1,19 @@
+import OpenAI from 'openai';
 
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY // This is the default and can be omitted
+});
 
+export async function GET() {
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'user',
+				content: 'Give me a random fun fact that is no more than 400 chars long.',
+			}
+		],
+		model: 'gpt-3.5-turbo'
+	});
+
+	return new Response(JSON.stringify(chatCompletion.choices.at(0)?.message.content), { status: 200 });
+}
