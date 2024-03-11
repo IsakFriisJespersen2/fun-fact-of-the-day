f7471da48377252a1a37e1951497e9ab611f43d1
Isak Friis-Jespersen <isak.friisjespersen2@gmail.com>
2024-03-11 08:05:58 +0100

    Storing old messages

--- src/routes/api/get-fact/+server.ts
+++ src/routes/api/get-fact/+server.ts
@@ -4,16 +4,23 @@ const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY // This is the default and can be omitted
 });
 
+let x = 0;
+let messages: (string | undefined | null)[] = [];
+
 export async function GET() {
 	const chatCompletion = await openai.chat.completions.create({
 		messages: [
 			{
-				role: 'user',
-				content: 'Give me a random fun fact that is no more than 400 chars long.',
+				role: 'assistant',
+				content: `Give me a random fun fact that is no more than 400 chars long. And it should not be one of these facts ${messages.join(', ')}`
 			}
 		],
-		model: 'gpt-3.5-turbo'
+		model: 'gpt-4'
 	});
 
-	return new Response(JSON.stringify(chatCompletion.choices.at(0)?.message.content), { status: 200 });
+	messages.push(chatCompletion.choices.at(0)?.message.content);
+
+	return new Response(JSON.stringify(chatCompletion.choices.at(0)?.message.content), {
+		status: 200
+	});
 }
