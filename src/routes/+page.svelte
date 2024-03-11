82934e954a35abf3b30a528aa6613016026f4c1e
Isak Friis-Jespersen <isak.friisjespersen2@gmail.com>
2024-03-11 10:16:03 +0100

    Added source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import Helix from '$lib/helix.svelte';
 	import { browser } from '$app/environment';
 
-	$: fact = null as string | null;
+	$: data = null as { fact: string; source: string } | null;
 	let isLoading = false;
 
 	let themes = [
@@ -16,7 +16,7 @@
 		{ title: 'Art', active: false }
 	];
 
-	const getFact = async (body: string[] = []): Promise<string> => {
+	const getFact = async (body: string[] = []): Promise<{ fact: string; source: string }> => {
 		isLoading = true;
 		const res = await fetch('/api/get-fact', {
 			method: 'POST',
@@ -38,10 +38,12 @@
 		const activeThemes = themes.filter((t) => t.active).map((t) => t.title);
 		if (activeThemes.length > 0) {
 			getFact(activeThemes).then((fetchedFact) => {
-				fact = fetchedFact;
+				data = fetchedFact;
 			});
 		}
 	}
+
+	console.log(data);
 
 	// Function to toggle theme active state and trigger reactivity
 	function toggleTheme(index: number) {
@@ -50,7 +52,7 @@
 	}
 
 	onMount(async () => {
-		fact = await getFact();
+		data = await getFact();
 	});
 </script>
 
@@ -61,9 +63,16 @@
 	</header>
 	<section class="w-1/2 h-96 max-w-4xl">
 		<div class="font-mono">
-			{#if !isLoading}
+			{#if !isLoading && !!data}
 				<div class="px-6 py-8 border rounded-md">
-					<p class="text-lg">{fact}</p>
+					<p class="text-lg">{data.fact}</p>
+					<div class="text-end mt-2 text-blue-300">
+						<a
+							href={`${data.source.substring(data.source.indexOf('(') + 1, data.source.indexOf(')'))}`}
+						>
+							{data.source.substring(data.source.indexOf('['), data.source.indexOf(']') + 1)}</a
+						>
+					</div>
 				</div>
 				<p class="flex justify-end font-thin mt-2">- Chat GPT</p>
 			{:else}
@@ -81,7 +90,7 @@
 					type="button"
 					data-active={theme.active}
 					on:click={() => toggleTheme(i)}
-					class="border rounded-md p-2 hover:bg-orange-700 data-[active=true]:bg-orange-700"
+					class="border rounded-md p-2 hover:bg-orange-700 active: data-[active=true]:bg-orange-700"
 					>{theme.title}</button
 				>
 			{/each}
--- src/routes/api/get-fact/+server.ts
+++ src/routes/api/get-fact/+server.ts
@@ -13,15 +13,24 @@ export async function POST({ request }: { request: Request }) {
 		messages: [
 			{
 				role: 'assistant',
-				content: `Give me a random fun fact that is no more than 400 chars long. The fact should be about one of the following themes ${body.themes.join(', ')} And it should not be one of these facts ${messages.join(', ')}`
+				content: `- Task: (Give me a random fun fact that is no more than 400 chars.
+				 The fact should be about one of the following themes ${body.themes.join(', ')}
+				 and it should not be one of these facts ${messages.join(', ')}).
+				 - Response format: (
+				 The response should be in JSON format and be separated into fact and source.
+				 Eg. { "fact": "The first oranges weren't orange", "source": "[<Give me a good short descriptive name of the source>] (URL)" }
+				 The response must always include fact and source fields and source must always have the following format: [name] (URL)
+				 )
+				 `
 			}
 		],
-		model: 'gpt-4'
+		model: 'gpt-3.5-turbo'
 	});
 
 	messages.push(chatCompletion.choices.at(0)?.message.content);
+	console.log(chatCompletion.choices.at(0)?.message.content);
 
-	return new Response(JSON.stringify(chatCompletion.choices.at(0)?.message.content), {
+	return new Response(chatCompletion.choices.at(0)?.message.content, {
 		status: 200
 	});
 }
